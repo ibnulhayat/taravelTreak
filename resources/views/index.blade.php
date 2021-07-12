2b38b9538edb374619efe0a6aa4323e5651da45b
ibnulhayat <ibnulhayatkhan@gmail.com>
2021-07-12 22:09:45 +0600

    jQuery plugin added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,15 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- Tell the browser to be responsive to screen width -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!-- Favicon icon -->
+	<link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/graduation-cap.png')}}">
+	<title>TT - @yield('pageTitle')</title>
 
 	<link rel="stylesheet" href="{{asset('assets/plugins/bootstrap-4.6.0/css/bootstrap.min.css')}}">
 	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
@@ -42,12 +49,26 @@
 					<h1 class="text-dark">Hello</h1>
 				</div>
 				<div class="col-3 ">
-					<h1 class="text-dark">Hello</h1>
+					<input type="text" id="name" >
+					<h1 id="submit" type="button" class="text-dark">Hello</h1>
+
 				</div>	
 			</div>
 		</div>
 	</div>
 
 	<script src="{{asset('assets/plugins/bootstrap-4.6.0/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/jQuery/jquery.min.js')}}"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+
+			$('#submit').click(function(event) {
+				var data = $('#name').val();
+				alert("Input : "+data);
+			});
+
+		});
+	</script>
 </body>
 </html>
