68ceeeda4a046ce59742a6d488d9c4d11d115ce3
ibnulhayat <ibnulhayatkhan@gmail.com>
2021-07-12 14:04:58 +0600

    update some resource

---
+++ resources/views/index.blade.php
@@ -0,0 +1,53 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+
+	<link rel="stylesheet" href="{{asset('assets/plugins/bootstrap-4.6.0/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+	
+</head>
+<body>
+
+	<div class="container-flude mt-5 bg-dark">
+		<div class="card bg-light">
+			<div class="row ">
+				<div class="col-3 ">
+					<button type="button" class="btn btn-outline-success btn-md ">Large button</button>
+				</div>
+				<div class="col-3 ">
+					<h1 class="text-dark">Hello</h1>
+				</div>
+				<div class="col-3 ">
+					<h1 class="text-dark">Hello</h1>
+				</div>
+				<div class="col-3 ">
+					<h1 class="text-dark">Hello</h1>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div class="container-flude m-t-40 bg-dark">
+		<div class="card bg-light">
+			<div class="row ">
+				<div class="col-3 ">
+					<button type="button" class="btn btn-outline-success btn-md ">Large button</button>
+				</div>
+				<div class="col-3 ">
+					<h1 class="text-dark">Hello</h1>
+				</div>
+				<div class="col-3 ">
+					<h1 class="text-dark">Hello</h1>
+				</div>
+				<div class="col-3 ">
+					<h1 class="text-dark">Hello</h1>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<script src="{{asset('assets/plugins/bootstrap-4.6.0/js/bootstrap.min.js')}}"></script>
+</body>
+</html>
